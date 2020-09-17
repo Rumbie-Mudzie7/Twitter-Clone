efb5d8ab5cf3a12bea429824ef39cb36b15cd2bb
rumbie <rumbiemudzie@gmail.com>
2020-09-17 21:45:16 +0200

    Add user_id to tweeets migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_16_211047) do
+ActiveRecord::Schema.define(version: 2020_09_16_215159) do
 
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
